c26df89e716f1ff8659c63061b4673ad50b22cc6
J0sH-code <joshrylesanteno07@gmail.com>
2026-01-28 14:45:02 +0800

    Incorporated hashmap to my Store-Product Aggregation file for practice

--- src/Aggregation.java
+++ src/Aggregation.java
@@ -1,25 +1,26 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Aggregation {
     static Scanner scanner = new Scanner(System.in);
+    static String storeName;
+    static Store myStore = new Store(storeName);
     public static void main(String[] args) throws Exception {
         runProgram();
     }
 
     static void runProgram() {
+        System.out.print("Enter store name: ");
+        storeName = scanner.nextLine();
+        
+
         boolean running = true;
         while (running) { 
-            storeProductRelation(running);
+            storeProductRelation(running, storeName, myStore);
         }
     }
 
-    static void storeProductRelation(boolean running) {
-        System.out.print("Enter store name: ");
-        String storeName = scanner.nextLine();
-        Store myStore = new Store(storeName);
-
+    static void storeProductRelation(boolean running, String storeName, Store myStore) {
         System.out.println("Welcome to " + storeName);
         System.out.println("[1: View Products] [2: Add Products] [3: Buy Product] [4: Remove Products]");
         int input = scanner.nextInt();
@@ -31,14 +32,13 @@ public class Aggregation {
     static void inputHandler(int input, Store store, boolean running){
         switch (input) {
             case 1 -> {
-                for (Product product : store.products) {
-                    System.out.println(product);
-                }
+                System.out.println(store.products.keySet());
             }
             case 2 -> {
                 System.out.println("Product name: ");
                 String productName = scanner.nextLine();
                 store.addProduct(new Product(productName.toLowerCase(), store));
+                System.out.println(store.products.keySet());
             }
             case 3 -> {
                 System.out.println("TBC");
@@ -46,7 +46,7 @@ public class Aggregation {
             case 4 -> {
                 System.out.print("Enter product name to remove: ");
                 String productName = scanner.nextLine();
-                store.removeProduct(new Product(productName, store));
+                store.removeProduct(productName);
             }
         
             default -> {
@@ -54,32 +54,47 @@ public class Aggregation {
             }
         }
     }
-
 }
 
 class Store {
     //Class Product is used an attribute for class Store
     String storeName;
-    ArrayList<Product> products = (ArrayList<Product>) Arrays.asList(new Product("dove", this), new Product("safeguard", this));
+    HashMap<String, Product> products = new HashMap<>();
 
     public Store (String storeName) {
         this.storeName = storeName;
+
+        setProducts();
+    }
+
+    private void setProducts() {
+        products.put("dove", new Product("dove", this));
+        products.put("safeguard", new Product("safeguard", this));
+        products.put("colgate", new Product("colgate", this));
     }
 
     void addProduct(Product product){
-        products.add(product);
+        products.put(product.getProductName(), product);
     }
 
-    void removeProduct(Product product){
-        products.remove(product);
+    void removeProduct(String productKey){
+        products.remove(productKey);
     }
 
 }
 
 class Product {
     //Class Store is used an attribute for class Product
-    private String productName;
+    private final String productName;
     private Store store;
+
+    public String getProductName() {
+        return this.productName;
+    }
+
+    public Store getStore() {
+        return this.store;
+    }
 
     public Product (String productName, Store store) {
         this.productName = productName;
