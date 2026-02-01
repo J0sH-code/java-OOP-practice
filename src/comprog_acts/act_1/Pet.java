e29129c7bfeea55aa6412922a9e6355b4c905dec
J0sH-code <joshrylesanteno07@gmail.com>
2026-02-01 22:33:39 +0800

    Added fields to Pet.java

--- src/comprog_acts/act_1/Pet.java
+++ src/comprog_acts/act_1/Pet.java
@@ -1,5 +1,13 @@
 package comprog_acts.act_1;
 
 public class Pet {
+    String name;
+    String breed;
+    Veterinarian vet = new Veterinarian();
 
+    public Pet (String name, String breed, Veterinarian vet) {
+        this.name = name;
+        this.breed = breed;
+        this.vet = vet;
+    }
 }
