c000f09eb48b9b267dc51bc60d7662be9e6b8a65
J0sH-code <joshrylesanteno07@gmail.com>
2026-02-02 23:17:05 +0800

    Set the Pet and Veterinarian properties and methods

--- src/comprog_acts/act_1/Pet.java
+++ src/comprog_acts/act_1/Pet.java
@@ -1,13 +1,55 @@
 package comprog_acts.act_1;
 
 public class Pet {
+    static int petCount = 0;
+
+    int healthLevel = 70;
+    int hungerLevel = 40;
+    int treatmentCount = 0;
+    
+    boolean isSick = false;
+    
     String name;
     String breed;
-    Veterinarian vet = new Veterinarian();
+    Veterinarian vet;
 
-    public Pet (String name, String breed, Veterinarian vet) {
+    public Pet(String name, String breed, Veterinarian vet) {
         this.name = name;
         this.breed = breed;
         this.vet = vet;
+
+        Pet.petCount++;
+
+        healthCeiling();
+    }
+    private void decreaseHealth(int value) {
+        this.healthLevel-=value;
+    }
+
+    private void increaseHunger (int value) {
+        this.hungerLevel+=value;
+    }
+
+    private void healthCeiling () {
+        this.healthLevel = (this.healthLevel > 100) ? 100 : this.healthLevel;
+    }
+
+    void play() {
+        increaseHunger(10);
+        decreaseHealth(15);
+    }
+
+    void isSick() {
+        decreaseHealth(20);
+        this.isSick = true;
+        this.treatmentCount++;
+    }
+
+    void increaseHealth(int value) {
+        this.healthLevel+=value;
+    }
+
+    void decreaseHunger (int value) {
+        this.hungerLevel-= value;
     }
 }
--- src/comprog_acts/act_1/Veterinarian.java
+++ src/comprog_acts/act_1/Veterinarian.java
@@ -1,5 +1,36 @@
 package comprog_acts.act_1;
 
-public class Veterinarian {
+import java.util.HashMap;
 
+public class Veterinarian {
+    static int vetCount = 0;
+    String name;
+    String specialization;
+    int yearsOfExperience;
+    int stressLevel = 50;
+    
+    HashMap<String, Pet> assignedPets = new HashMap<>();
+
+    public Veterinarian(String name, String specialization, int yearsOfExperience) {
+        this.name = name;
+        this.specialization = specialization;
+        this.yearsOfExperience = yearsOfExperience;
+        Veterinarian.vetCount++;
+    }
+
+    void treatPet (Pet pet) {
+        pet.vet = this;
+        pet.increaseHealth(25);
+        pet.decreaseHunger(10);
+        assignPet(pet);
+        checkPetHealth(pet);
+    }
+
+    private void assignPet (Pet pet) {
+        assignedPets.put(pet.name, pet);
+    }
+
+    private void checkPetHealth (Pet pet) {
+        this.stressLevel = (pet.healthLevel >= 100) ? this.stressLevel-=10 : this.stressLevel;
+    }
 }
